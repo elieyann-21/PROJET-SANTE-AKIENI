d566cc6118d51537e82f611bc09922160c6ecdb5
elieyann-21 <elieyannongouya@gmail.com>
2026-07-10 10:14:21 +0100

    Create semaine5_exercice_calculatrice.py

---
+++ semaine5/semaine5_exercice_calculatrice.py
@@ -0,0 +1,69 @@
+# ======================================================
+# AKIENI ACADEMY
+# SEMAINE 5 - EXERCICE : CALCULATRICE 
+# DESCRIPTION : creer une calculatrice qui permet d'effectuer les 4 operations de base
+# addition, soustraction, multiplication et division.
+# ======================================================
+
+# choix de l'operation par l'utilisateur
+print("="*70)
+print("Bonjour\n Bienvenue dans votre calcultatrice")
+print("-"*70)
+print("Choisissez parmis les operations suivants pour réaliser votre opération\n add : Addition\n sous: Soustraction\n mul: Multiplication\n div: Division")
+print("-"*70)
+operateur = str(input("Saisissez l'opérateur :"))
+
+# vérifie si l'opérateur saisit correspond à un opérateur valide
+while operateur not in ['add','sous','mul','div']:
+        print("Erreur : L'operateur saisit doit correspondre a celui du menu")
+        operateur = str(input("Saisissez l'opérateur :"))
+
+# définition de la fonction d'addition
+def addition():
+    num1 = float(input("chiffre 1 :"))
+    num2 = float(input("chiffre 2 :"))
+    addition = num1 + num2
+    print("Résultat :",addition)
+
+# définition de la fonction de soustraction
+def soustraction():
+    num1 = float(input("chiffre 1 :"))
+    num2 = float(input("chiffe 2 "))
+    soustraction = num1 - num2
+    print("Résultat :", soustraction)
+
+# définition de la fonction de multification
+def multiplication():
+    num1 = float(input("chiffre 1 :"))
+    num2 = float(input("chiffre 2 :"))
+    multiplication = num1 * num2
+    print("Résultat :", multiplication)
+
+# définition de la fonction de division
+def division():
+    num1 = float(input("chiffre 1"))
+    num2 = float(input("chiffre 2 :"))
+    while num2 == 0: # pour le cas où le dénominateur est 0
+        print("Erreur : 0 ne peut etre un diviseur\n Veuillez saisir une nouvelle valeur")
+        num2 = float(input("chiffre 2 :"))
+    division = num1/num2
+    print('Divion :', division)
+
+# Execution de la calculatrice
+if operateur == "add":
+    print("Opération : addition")
+    addition()
+elif operateur == "sous":
+    print("Opération : soustraction")
+    soustraction()
+elif operateur == "mul":
+    print("Opération : multiplication")
+    multiplication()
+else:
+    print("Opération : division")
+    division()
+
+       
+
+
+
